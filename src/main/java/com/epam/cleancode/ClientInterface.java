4f7f06238dfa3b3be856a29dc5a0bbedb6bb383b
Usha-29 <ravuri.saiusha@gmail.com>
2020-02-24 21:46:48 +0530

    first commit

--- src/main/java/com/epam/cleancode/ClientInterface.java
+++ src/main/java/com/epam/cleancode/ClientInterface.java
@@ -1,22 +1,27 @@
 package com.epam.cleancode;
 import java.util.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 public class ClientInterface {
+	 private static final Logger LOGGER=LogManager.getLogger(ClientInterface.class);
 	public static void main(String args[]) {
           Scanner sc=new Scanner(System.in);
-		 PrintStream out =  new PrintStream(new FileOutputStream(FileDescriptor.out));
-	   
+          PrintStream myout =  new PrintStream(new FileOutputStream(FileDescriptor.out));
+          LOGGER.info("Using Logger");
+          
 	    
 	    int p; // P	=	initial principal balance
 		double r; // r   =	interest rate
 		int n;   // n	=   number of times interest applied per time period
 		int t;   // t	=	time in years
 		
-		out.print("To calculate Simple Interest and Compound Interest\n\n");
-		out.print("Enter Principal amount(P) \nRate of Interest(r/100) \nTime in years(t) \nNumber of times interest applied per time period(n)\n");
+		myout.print("\nTo calculate Simple Interest and Compound Interest");
+		myout.print("\n\nEnter Principal amount(P) \nRate of Interest(r/100) \nTime in years(t) \nNumber of times interest applied per time period(n)\n");
 
 	    p=sc.nextInt(); 
 	    r=sc.nextDouble();
@@ -25,23 +30,24 @@ public class ClientInterface {
 	    
 	    Simple_Compound_Interest interest=new  Simple_Compound_Interest(p,r,t,n);
 	    
-	   out.print("\nSimple Interest is:"+ interest.SimpleInterest());
-	    out.print("\nCompound Interest is:"+ interest.CompoundInterest());
+	    myout.print("Simple Interest is:"+ interest.SimpleInterest());
+	    myout.print("\n\n");
+	    myout.print("Compound Interest is:"+ interest.CompoundInterest());
 	    
 	    
-	    out.print("\n\n\nHouse Construction\n\n");
 	    
-	    out.print("Enter Total area \nStandard(1-standard  2-above standard  3-high standard)\n");
+	    myout.print("\n\n\nHouse Construction");
+	    myout.print("\n\nEnter Total area and Standard(1-standard  2-above standard  3-high standard):\n");
 	    double total_area =sc.nextDouble();
 		int standard=sc.nextInt(); 
-		out.print("Do you want a Fully Automated Home??\n If yes press 1 else 0 ");
+		myout.print("\nDo you want a Fully Automated Home??\n If yes press 1 else 0:\n ");
 		int press=sc.nextInt();
 		boolean fully_automated_home= press==1? true:false;
 		
 		Construction_Cost cc=new Construction_Cost(total_area,standard,fully_automated_home);
-		out.print("Construction Cost Of Home is:"+ cc.estimate());
-		
-	   
+		myout.print("Construction Cost Of Home is:"+ cc.estimate());
+		  myout.print("\n\n");
+		 LOGGER.info("Exiting Program..");
 	    sc.close();
 	}
 
--- src/main/java/com/epam/cleancode/Construction_Cost.java
+++ src/main/java/com/epam/cleancode/Construction_Cost.java
@@ -1,11 +1,16 @@
 package com.epam.cleancode;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Construction_Cost {
+	 private static final Logger LOGGER=LogManager.getLogger(Construction_Cost.class);
 	double total_area;
 	int standard;
 	boolean fully_automated_home;
 	
 	 Construction_Cost (double area,int std, boolean fully_automated_home){
+		 LOGGER.info("Intialized parameters.");
 		 total_area=area;
 		 standard=std;
 		 this.fully_automated_home=fully_automated_home;
@@ -13,6 +18,7 @@ public class Construction_Cost {
 	 }
 	 
 	 double estimate() {
+		 LOGGER.info("Entered estimate method.\n");
 		 double res=1;
 		 if(standard==1)
 			 res=1200*total_area;
